df0580192b2c318ef094328cf66beb6721831bb2
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2023-12-03 22:25:37 +0700

    add: fe review

--- post_properti/urls.py
+++ post_properti/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # other patterns...
     path('', show_all_posts, name='show_all_posts'),
     path('<int:id>/', show_post_detail, name='show_post_detail'),
-    path('<int:id>/', include('reviewrating.urls')),
+    path('<int:id>/reviews/', include('reviewrating.urls')),
     path('add_post/', add_post, name='add_post'),
     path('edit_post/<int:post_id>/', edit_post, name='edit_post'),
     path('search_post/', search_post_by_name, name='search_post'),
