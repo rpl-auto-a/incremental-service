f64832fe0d78327504cda2a8050880533d71341d
danenherdi <danendra.herdiansyah@ui.ac.id>
2023-12-04 01:08:15 +0700

    fixing BE and add FE for edit post and search post by name

--- post_properti/forms.py
+++ post_properti/forms.py
@@ -4,7 +4,7 @@ from .models import PostProperti
 class PostPropertiForm(forms.ModelForm):
     class Meta:
         model = PostProperti
-        fields = ['nama_properti', 'deskripsi_properti', 'foto_properti', 'kota_properti', 'negara_properti', 'kode_pos_properti']
+        fields = ['nama_properti', 'deskripsi_properti', 'foto_properti', 'negara_properti', 'kota_properti', 'kode_pos_properti']
 
 class FilterForm(forms.Form):
     negara = forms.ChoiceField(label='Negara', choices=[])
--- post_properti/urls.py
+++ post_properti/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('new/', new_post, name='new_post'),
     path('<int:id>/reviews/', include('reviewrating.urls')),
     path('add_post/', add_post, name='add_post'),
-    path('edit_post/<int:post_id>/', edit_post, name='edit_post'),
+    path('edit_post/<int:id>/', edit_post, name='edit_post'),
     path('search_post/', search_post_by_name, name='search_post'),
     path('json/', all_posts_json, name='all_posts_json'),
     path('user_posts/', show_user_posts, name='show_user_posts'),
