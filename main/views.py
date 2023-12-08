99c60e9a726d9afa3d8b08ca38f3180696ba8630
Alanna <alanna@ui.ac.id>
2023-12-08 21:15:29 +0700

    Landing page redirects to all posts

--- main/views.py
+++ main/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from userData.models import UserData
 
 # Create your views here.
+# def show_main(request):
+#     context = {"user": "Guest"}
+
+#     if request.user.is_authenticated:
+#         user_data = UserData.objects.get(user=request.user)
+
+#         context["user"] = request.user
+#         context["user_data"] = user_data
+        
+#     return render(request, "main.html", context)
+
 def show_main(request):
     context = {"user": "Guest"}
 
@@ -11,4 +22,4 @@ def show_main(request):
         context["user"] = request.user
         context["user_data"] = user_data
         
-    return render(request, "main.html", context)
+    return redirect('show_all_posts')
